02142a7ed8b89d783b972ff54dc009a958719efc
kimgogo <vincent.guangzhi@gmail.com>
2026-05-29 17:58:35 +0800

    Add new renewal URL to RENEW_URLS list

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ except ImportError:
 RENEW_URLS = [
     "https://host2play.gratis/server/renew?i=090fe51f-445b-4864-a745-bb063363656e",
     "https://host2play.gratis/server/renew?i=de352ef0-a45f-4d53-bd9c-420055b8b408",
+    "https://host2play.gratis/server/renew?i=b387cf8e-967e-467a-8583-7db159a21603",
     # 添加更多链接
 ]
 
